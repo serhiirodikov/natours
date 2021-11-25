92fb6abe3c5a1111b4c82d4d79fbc46950a72708
serhiirodikov <sergiy1998373@gmail.com>
2021-11-25 23:51:29 +0200

    Added scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,11 @@ import logo from "../images/logo-white.png";
 import { ButtonMain } from "./ButtonMain";
 
 export const Header = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    document.querySelector("#tours").scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <header className="header">
       <div className="header__logo-box">
@@ -12,7 +17,7 @@ export const Header = () => {
           <span className="heading-primary--main">OUTDOORS</span>
           <span className="heading-primary--sub">IS WHERE LIFE HAPPENS</span>
         </h1>
-        <ButtonMain color="white" animated>
+        <ButtonMain color="white" animated onClick={handleClick}>
           discover our tours
         </ButtonMain>
       </div>
